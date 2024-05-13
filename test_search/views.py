9e22390cdcda5c07279642a3c1b5a3f2ed448679
Minhojkhon <minhojkhon@icloud.com>
2024-05-13 11:19:49 +0300

    added template to gitignore

--- test_search/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse_lazy, reverse
-
-
-def index(request):
-    id = 1
-    return HttpResponse(id)
